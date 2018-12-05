2302d42043b712d016151dd7ee8b2094dfed7a55
Kadri Rootalu <kadri.rootalu@ut.ee>
2018-12-05 13:37:40 +0200

    Add files via upload

---
+++ R_Test.R
@@ -0,0 +1,10 @@
+#Opening the data-set
+
+#Name of the data-set: MyData
+
+MyData <- read.csv2('fsQCA_test_R.csv')
+
+hist (MyData$ZSUBJ_SICHER_fe_fs)
+
+plot(MyData$ZSUBJ_SICHER_fe_fs, MyData$AFFEKTION_fe_fs, main  = "Scatter SuSE und Affektion",
+     xlab="SuE ", ylab="Affektion ", pch=18)
